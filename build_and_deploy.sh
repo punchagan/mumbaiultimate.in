ec193662947d60ee912ccd86128e929a31c3215a
Puneeth Chaganti <punchagan@muse-amuse.in>
2014-01-21 19:22:13 +0530

    Add user info, token and update push url.

---
+++ build_and_deploy.sh
@@ -0,0 +1,28 @@
+set -e
+
+# Build
+nikola build
+
+# Setup git for pushing from Travis
+## Setup user details, so commits etc work.
+git config user.email "travis@testing.ci"
+git config user.name "Travis CI"
+## Change the remote url using the token
+git remote set-url --push origin https://github.com/punchagan/mumbaiultimate.in.git
+
+# Commit the Build output
+## Create a new gh-pages branch
+git branch -D gh-pages || true
+git checkout --orphan gh-pages
+## Remove all the source files, we only want the output!
+ls | grep -v output | xargs rm -rf
+mv output/* .
+## Remove deleted files
+git ls-files --deleted -z | xargs -0 git rm >/dev/null 2>&1
+## Add new files
+git add . >/dev/null 2>&1
+## Commit
+git commit -m "$(date)"
+
+# Push!
+git push -f origin gh-pages:gh-pages
